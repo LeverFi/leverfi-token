5d23ac60f6d0246efb9e239caa005e9d0477a58b
renz-leverfi <renz@leverfi.io>
2025-03-26 16:53:49 +0800

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Usage:
+# ./deploy.sh --aws : Simulate with AWS KMS
+# ./deploy.sh --broadcast --aws : Execute with AWS KMS
+# ./deploy.sh --broadcast --private-key <RAW_PRIVATE_KEY> : Execute with private key
+
+forge clean && forge build
+./flatten.sh
+source .env && forge script scripts/deploy-lever.s.sol --slow --verifier etherscan --rpc-url $RPC_URL $@
