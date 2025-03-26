d8399b9034ae81967859baf1498341d503405ee9
renz-leverfi <renz@leverfi.io>
2025-03-26 17:28:37 +0800

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@
 # ./deploy.sh --broadcast --aws : Execute with AWS KMS
 # ./deploy.sh --broadcast --private-key <RAW_PRIVATE_KEY> : Execute with private key
 
-forge clean && forge build
-./flatten.sh
-source .env && forge script scripts/deploy-lever.s.sol --slow --verifier etherscan --rpc-url $RPC_URL $@
+#forge clean && forge build
+#./flatten.sh
+source .env && forge script \
+  scripts/deploy-lever.s.sol \
+  --slow \
+  --rpc-url $RPC_URL \
+  $@
