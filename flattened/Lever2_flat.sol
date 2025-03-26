ee8d407c5d45c636ee36c08f6dc88eedfefcdb82
renz-leverfi <renz@leverfi.io>
2025-03-26 16:53:39 +0800

    flatten Lever2.sol

--- flattened/Lever2_flat.sol
+++ flattened/Lever2_flat.sol
@@ -2033,11 +2033,12 @@ abstract contract ERC20PermitUpgradeable is Initializable, ERC20Upgradeable, IER
 // contracts/Lever2.sol
 
 /// @custom:security-contact security@leverfi.io
+/// @custom:oz-upgrades-from contracts/Lever.sol:Lever
 contract Lever2 is Initializable, ERC20Upgradeable, PausableUpgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
+    constructor() {}
 
-    function initialize() initializer public {
+    function initialize() public initializer {
         __ERC20_init("Lever", "LEVER");
         __Pausable_init();
         __Ownable_init();
